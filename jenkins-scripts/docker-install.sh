26770722c6b99fc805d20a769233c52f43fd189d
Reece-elder <RElder@academytrainee.com>
2020-10-30 15:00:14 +0000

    added if statements

--- jenkins-scripts/docker-install.sh
+++ jenkins-scripts/docker-install.sh
@@ -1,11 +1,9 @@
 #! /bin/bash
 
-sudo apt install -y curl
-curl https://get.docker.com | sudo bash
-sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo usermod -aG docker $(whoami)
-
 if ![which docker] ; then
-
+    sudo apt install -y curl
+    curl https://get.docker.com | sudo bash
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+    sudo usermod -aG docker $(whoami)
 fi
